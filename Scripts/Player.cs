e7b3ff2eccb5d0f0d495d684780360cb8c14108e
thebestasus <thebestandgreatest040404@gmail.com>
2024-09-12 22:28:55 -0600

    temp turret fix
    added crosshair, added a cursor offset, no idea how to fix the turret movement yet

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,48 +3,65 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	[Export]
-	public int Speed {get; set; } = 200;
-	[Export]
-	public int BodyRotateSpeed = 3;
-	[Export] 
-	public int TurretRotateSpeed = 3;
+	[Export] public int Speed {get; set; } = 200;
+	[Export] public int BodyRotateSpeed = 3;
+	[Export] public int TurretRotateSpeed = 3;
+	[Export] public bool UseController = false;
+	[Export] public int CursorXOffset = 19;
+	[Export] public int CursorYOffset = 14;
 
 	public Sprite2D TankTurret;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		//temp cursor change for aiming purposes
+		Input.SetCustomMouseCursor(ResourceLoader.Load("res://Assets/Sprites/crosshair007.png"));
 		TankTurret = GetNode<Sprite2D>("Tank Body/Tank Turret");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-		Velocity = inputDirection * Speed;
+		GetInput();
 		MoveAndSlide();
 		Animate();
+	}
+
+	public void GetInput()
+	{
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * Speed;
 	}
 
 	public void Animate()
 	{
 		//turret rotation
+		//see what has been used more recently mouse/keyboard movement or controller
+		//ignore the other options inputs
+		//get input vectors from other control stick
+
+		Vector2 mouse = new Vector2(GetGlobalMousePosition().X + CursorXOffset, GetGlobalMousePosition().Y + CursorYOffset);
+		TankTurret.GlobalRotationDegrees = Mathf.RadToDeg(GlobalPosition.AngleToPoint(mouse)) - 90;
+		
+		/*
+		not working, has an issue with bouncing between two angles and has a creeping inaccuracy
+		double turretAngle = (float)Math.Round(TankTurret.GlobalRotationDegrees);
+		double turretAngleDifference = 0;
 		double mouseAngle =
 			Math.Round(Mathf.RadToDeg(TankTurret.GlobalPosition.AngleToPoint(GetGlobalMousePosition()))) - 90;
-		var turretAngle = (float)Math.Round(TankTurret.GlobalRotationDegrees);
-		double turretAngleDifferent = AngleDifference(mouseAngle, turretAngle);
-		if (turretAngleDifferent > TurretRotateSpeed)
+		turretAngleDifference = AngleDifference(mouseAngle, turretAngle);
+		
+		if (turretAngleDifference > (TurretRotateSpeed * 2))
 		{
 			turretAngle -= TurretRotateSpeed;
 		}
-		else if (turretAngleDifferent < TurretRotateSpeed)
+		else if (turretAngleDifference < (TurretRotateSpeed * 2))
 		{
 			turretAngle += TurretRotateSpeed;
 		}
-		TankTurret.GlobalRotationDegrees = (float) Math.Round(turretAngle);
-		
-		
+		TankTurret.GlobalRotationDegrees = (float) Math.Floor(turretAngle);
+		*/
 
 		//body rotation
 		double velocityAngle = Mathf.Round(Mathf.RadToDeg(Velocity.Angle()));
